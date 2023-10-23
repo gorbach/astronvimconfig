342bfdc5bd01b72dc34120eef1ea70594d2cab0f
Gorbach Oleksii <gorbach.alexey@gmail.com>
2023-10-23 15:57:11 +0200

    use catppuccin-macchiato theme

--- init.lua
+++ init.lua
@@ -18,8 +18,10 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
 
+  -- colorscheme = "astrodark",
+  colorscheme = "catppuccin-macchiato",
+  
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
